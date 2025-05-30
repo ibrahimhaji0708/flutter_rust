fafb06a2ab350cff51a7364d791d3c0d7ee1ada2
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-05-30 16:02:57 +0530

    flutter+rust ffi_bridge

---
+++ rust/target/debug/deps/rust-218464727e1e6ea7.d
@@ -0,0 +1,5 @@
+/home/ibrahim/code/Rust_tutorials/flutter_rust/rust/target/debug/deps/librust-218464727e1e6ea7.rmeta: src/lib.rs
+
+/home/ibrahim/code/Rust_tutorials/flutter_rust/rust/target/debug/deps/rust-218464727e1e6ea7.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust/target/debug/deps/rust-c3c0c3e2c59ce488.d
@@ -0,0 +1,5 @@
+/home/ibrahim/code/Rust_tutorials/flutter_rust/rust/target/debug/deps/librust-c3c0c3e2c59ce488.rmeta: src/lib.rs
+
+/home/ibrahim/code/Rust_tutorials/flutter_rust/rust/target/debug/deps/rust-c3c0c3e2c59ce488.d: src/lib.rs
+
+src/lib.rs:
